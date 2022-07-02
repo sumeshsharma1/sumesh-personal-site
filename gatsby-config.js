047545bf9429d1564761f06eb57f8f7c01266c86
sumeshsharma1 <sumesharma1997@gmail.com>
2022-07-02 14:06:30 -0700

    First commit

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteMetadata: {
+    description: "Sumesh Sharma's Personal Website",
+    locale: "en",
+    title: "Sumesh Sharma",
+    // formspreeEndpoint: "https://formspree.io/f/{your-id}",
+  },
+  plugins: [
+    {
+      resolve: "@wkocjan/gatsby-theme-intro",
+      options: {
+        theme: "dark-blue",
+      },
+    },
+  ],
+}
