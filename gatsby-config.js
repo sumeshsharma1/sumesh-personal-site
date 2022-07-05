b7a962a620eec5fffe4c86ed98599219df8da03e
sumeshsharma1 <sumesharma1997@gmail.com>
2022-07-04 18:28:27 -0700

    Adding yarn

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     // formspreeEndpoint: "https://formspree.io/f/{your-id}",
   },
   plugins: [
+    `gatsby-plugin-sharp`,
     {
       resolve: "@wkocjan/gatsby-theme-intro",
       options: {
